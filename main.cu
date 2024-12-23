1e1a4ae39141d9ce7b25e50e901009e56f86765e
ffrapheal <147612914+ffrapheal@users.noreply.github.com>
2024-12-23 21:24:17 +0800

    Add files via upload

--- main.cu
+++ main.cu
@@ -17,21 +17,20 @@ int main()
     struct HashData hash;
     struct HashParams params;
     params.m_hashBucketSize=4;
-    params.m_hashMaxCollisionLinkedListSize=100;
-    params.m_hashNumBuckets=100;
+    params.m_hashMaxCollisionLinkedListSize=10;
+    params.m_hashNumBuckets=10;
     params.m_integrationWeightMax=0.5;
     params.m_integrationWeightSample=0.5;
     params.m_maxIntegrationDistance=10;
     params.m_numOccupiedBlocks=0;
-    params.m_numSDFBlocks=1000;
-    params.m_SDFBlockSize=8*8*8;
-    params.m_truncation=100;
-    params.m_truncScale=100;
-    params.m_virtualVoxelSize=100;
+    params.m_numSDFBlocks=100;
+    params.m_SDFBlockSize=4*4*4;
+    params.m_truncation=10;
+    params.m_truncScale=10;
+    params.m_virtualVoxelSize=0.1;
     hash.allocate(params,true);
-    dim3 blockSize(8,8);
-    dim3 gridSize(64);
-    test <<<gridSize,blockSize>>> (hash);
-    
+    dim3 blockSize(4,4);
+    dim3 gridSize(8);
+    hash.free();
     return 0;
 }
