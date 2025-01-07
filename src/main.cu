48a81c86f1023a319b4df7297e6097b4e748a1bb
haoming-Yu <1813442316@qq.com>
2025-01-07 16:09:20 +0800

    reformat

--- src/main.cu
+++ src/main.cu
@@ -2,25 +2,28 @@
 #include "VoxelHash.h"
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/io.h>
+
 __global__ void test(HashData * hash,int * count,float * pos,int * num_points)
 {
     int idx=blockIdx.x*blockDim.x+threadIdx.x;
     if(idx>=*num_points) return;
     float3 worldpos=make_float3(pos[idx*3],pos[idx*3+1],pos[idx*3+2]);
     //assume (a b c) is the coordinate of a point.
-    bool insert;
     int3 voxelpos = hash->worldToVirtualVoxelPos(worldpos);
     uint hashpos=hash->computeHashPos(worldpos);
-    insert=hash->insertHashEntryElement(worldpos);
+    hash->insertHashEntryElement(worldpos);
     __threadfence();
-    HashEntry curr = hash->getHashEntry(worldpos);
+    HashEntry curr = hash->getHashEntryForWorldPos(worldpos);
     uint h = hash->computeHashPos(worldpos);
     if(curr.ptr!=FREE_ENTRY)
     {
         atomicAdd(&count[0], 1);
-        Voxel v = hash->getVoxel(worldpos);
+        Voxel * v = hash->getVoxel(worldpos);
+        printf("voxelpos: %d %d %d\n",voxelpos.x,voxelpos.y,voxelpos.z);
+        printf("voxel: %f %f %f\n",v->sdf_sum,v->weight_sum,v->sdf_sum/v->weight_sum);
     }
     //printf("a:%d b:%d c:%d pos:%d ptr:%d insert:%s\n",voxelpos.x,voxelpos.y,voxelpos.z,pos,curr.ptr,insert ? "true" : "false");
     return;
@@ -33,33 +36,33 @@ void checkCudaError(cudaError_t err) {
 }
 
 int main() {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-    // 读取PCD文件
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/zzz/code/hash/point_cloud_7377_points.pcd", *cloud) == -1) // 这里替换为你的PCD文件路径
+    pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZINormal>);
+    printf("hello world\n");
+    // read pcd file
+    if (pcl::io::loadPCDFile<pcl::PointXYZINormal>("/home/hmy/voxel_hashing_dev/point_cloud_7377_points.pcd", *cloud) == -1) // 这里替换为你的PCD文件路径
     {
-        PCL_ERROR("无法读取文件 \n");
+        PCL_ERROR("failed to read pcd file \n");
         return -1;
     }
-    // 输出点云信息
-    std::cout << "点云宽度: " << cloud->width << std::endl;
-    std::cout << "点云高度: " << cloud->height << std::endl;
-    std::cout << "点云大小: " << cloud->points.size() << std::endl;
-    // 遍历点云中的每个点
+    // print point cloud info
+    std::cout << "point cloud width: " << cloud->width << std::endl;
+    std::cout << "point cloud height: " << cloud->height << std::endl;
+    std::cout << "point cloud size: " << cloud->points.size() << std::endl;
+    // traverse each point in point cloud
     size_t num_points = cloud->points.size();
-    float* host_points = new float[num_points * 3]; // 每个点有三个坐标
+    float* host_points = new float[num_points * 3]; // each point has three coordinates
     for (size_t i = 0; i < num_points; ++i) {
         host_points[i * 3] = cloud->points[i].x;
         host_points[i * 3 + 1] = cloud->points[i].y;
         host_points[i * 3 + 2] = cloud->points[i].z;
     }
 
-    // 分配GPU内存
+    // allocate memory on gpu
     float* device_points;
     cudaMalloc(&device_points, num_points * 3 * sizeof(float));
     int* device_num_points;
     cudaMalloc(&device_num_points,sizeof(int));
-    // 复制数据到GPU
+    // copy data to gpu
     cudaMemcpy(device_points, host_points, num_points * 3 * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(device_num_points, &num_points, sizeof(int), cudaMemcpyHostToDevice);
     delete[] host_points;
